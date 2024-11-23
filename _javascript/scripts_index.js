d1c09a7d537895d7fd20e53eec7d432ca61afe11
Heber F Barra <heberferreirabarra@gmail.com>
2024-11-23 12:22:57 -0300

    Primeira versão botão dark mode

--- _javascript/scripts_index.js
+++ _javascript/scripts_index.js
@@ -1,16 +1,43 @@
-let toggleButton = document.getElementsByTagName("button")[0];
-let toggle = true;
+let btns = document.getElementsByTagName("button");
+let root = document.querySelector(":root");
+let toggleMenuFollow = true;
+let darkTheme = false
 
-toggleButton.addEventListener("click", function() {
+btns[0].addEventListener("click", function() {
     let header = document.getElementsByTagName("header")[0];
-    if (toggle) {
+    if (toggleMenuFollow) {
         header.style.position = "relative";
-        toggleButton.innerText = "Voltar a seguir";
-        toggle = false;
+        btns[0].innerText = "Voltar a seguir";
+        toggleMenuFollow = false;
     } else {
         header.style.position = "sticky";
-        toggle = true;
-        toggleButton.innerText = "Parar de seguir";
+        toggleMenuFollow = true;
+        btns[0].innerText = "Parar de seguir";
+    }
+}
+)
+
+btns[1].addEventListener("click", function() {
+    if (darkTheme) {
+        root.style.setProperty('--border_color', 'white');
+        root.style.setProperty('--item_background_color', 'rgb(80, 80, 87)');
+        root.style.setProperty('--alternative_background_color', 'lightgray');
+        root.style.setProperty('--body_color', 'rgb(53, 52, 52)');
+        root.style.setProperty('--shadow_color', 'rgba(199, 193, 193, 0.4)');
+        root.style.setProperty('--link_color', 'aquamarine');
+        root.style.setProperty('--alternative_link_color', 'cyan');
+        root.style.setProperty('--global_font_color', 'white');
+        darkTheme = false;
+    } else {
+        root.style.setProperty('--border_color', 'gray');
+        root.style.setProperty('--item_background_color', 'white');
+        root.style.setProperty('--alternative_background_color', 'lightgray');
+        root.style.setProperty('--body_color', 'lightblue');
+        root.style.setProperty('--shadow_color', 'rgba(0, 0, 0, 0.4)');
+        root.style.setProperty('--link_color', 'lightblue');
+        root.style.setProperty('--alternative_link_color', 'blue');
+        root.style.setProperty('--global_font_color', 'black');
+        darkTheme = true;
     }
 }
 )
