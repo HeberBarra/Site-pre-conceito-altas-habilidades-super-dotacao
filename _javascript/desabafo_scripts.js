efac16ed10b47622ee39f328b545824e7813da2e
Heber F Barra <heberferreirabarra@gmail.com>
2024-11-23 12:24:58 -0300

    correção de bug de tema

--- _javascript/desabafo_scripts.js
+++ _javascript/desabafo_scripts.js
@@ -1,6 +1,9 @@
+const sendForm = document.getElementsByTagName("form")[0];
 const textAreaInput = document.getElementsByTagName("textarea")[0];
 const wordNum = document.getElementById("word_num");
 const formWithButtonsTheme = document.getElementsByName("theme")[0];
+
+sendForm.action = location.href
 
 textAreaInput.addEventListener("input", () => {
     wordNum.innerText = textAreaInput.textLength;
