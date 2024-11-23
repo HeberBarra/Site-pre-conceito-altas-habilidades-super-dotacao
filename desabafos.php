556198e2993c689e2c01307b71cee7e95a54fc11
Heber F Barra <heberferreirabarra@gmail.com>
2024-11-23 12:23:24 -0300

    refiz um pouco da parte da página dos desabafos

--- desabafos.php
+++ desabafos.php
@@ -46,52 +46,44 @@
         }
 
         body{
-            display: flex;
-            flex-direction: column;
             background-color: var(--body_color);
             color: var(--global_font_color);
             margin: 0;
             width: 100%;
             height: 100%;
-            justify-content: center;
-            align-items: center;
         }
 
     </style>
-    <link rel="stylesheet" href="_css/desabafos_mobile.css">
+    <script src="_javascript/global_scripts.js" defer></script>
+    <link rel="stylesheet" href="_css/desabafos.css">
 </head>
 <body>
     <header>
         <nav id="menu">
             <div class="menu-item">
-                <a href="index.html">
+                <a href="index.html" class="menu-link">
                     <span>Home</span>
                 </a>
             </div>
             <div class="menu-item">
-                <a href="fale_conosco.html">
+                <a href="fale_conosco.html" class="menu-link">
                     <span>Fale Conosco</span>
                 </a>
             </div>
         </nav>
     </header>
+    <h1>Desabafos</h1>
     <main>
         <article>
             <p>Esta área do site foi construída para quem quiser desabafar ou relatar algum preconceito sofrido por alguém por ele ter Altas Habillidades/Super Dotação, mesmo que eu, o criador deste site, não tenha sofrido preconceito até ao dia de hoje por ter Altas Habilidades/Superdotação, eu considero de suma de importância que exista um lugar para que as pessoas possam desabafar e relatar esses acontecimentos. Há também a opção de responder essas histórias, num sistema similar ao da seção de comentários do Youtube. Por favor, seja gentil e empático, faça o mal e terá tribulação, faça o bem e terá paz.</p>
         </article>
-        <div id="desabafos">
-            <?php
-                $total_page_number = 1;
-                $page_number = 1;
-                $comment_number = 1;
-                echo "<div id='inicio'><p>Desabafos | Página de $page_number de $total_page_number</p></div>";
-                for ($c = 1; $c<=10; $c++) {
-                    echo "<h2>Fulano <span>#$comment_number</span></h2>";
-                    echo "<p class='comentario'>Desabafo de teste</p>";
-                }
-                echo "<div id='seletor_de_pagina'><button>Voltar</button>$page_number $total_page_number<button>Próxima Página</button></div>"
-            ?>
-        </div>
     </main>
+    <div id="desabafos">
+        <?php
+            echo "<button>Voltar</button>";
+            printf("%d ... %d", 1, 2);
+            echo "<button>Próxima página</button>";
+        ?>
+    </div>
 </body>
 </html>
